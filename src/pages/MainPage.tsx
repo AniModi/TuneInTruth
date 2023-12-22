30b92561f175f87be3a4aab802e920fc8bc96e35
AniModi <98632268+AniModi@users.noreply.github.com>
2023-12-22 13:32:38 +0530

    Modified main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./MainPage.css";
-import { Grid } from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
 
 interface MainPageProps {
   setPage: React.Dispatch<React.SetStateAction<number>>;
@@ -14,13 +14,12 @@ interface NewsArticle {
   articleLink: string;
 }
 
-// ...
 
 const MainPage: React.FC<MainPageProps> = ({ setPage }) => {
-  const [isLoggedin, setIsLoggedin] = useState(false);
-  const [newsArticles, setNewsArticles] = useState<NewsArticle[]>([]);
   const [loading, setLoading] = useState(true);
-  
+  const [articleLink, setArticleLink] = useState<string>("");
+  const [newsArticle, setNewsArticle] = useState<NewsArticle | null>(null);
+
   useEffect(() => {
     async function checkToken() {
       const token: any = await chrome.storage.sync.get(["token"]);
@@ -45,103 +44,111 @@ const MainPage: React.FC<MainPageProps> = ({ setPage }) => {
         setPage(1);
         return;
       }
-
-      setIsLoggedin(true);
     }
 
     checkToken();
-  }, [setPage, setIsLoggedin]);
+  }, [setPage]);
 
-  useEffect(() => {
-    const delay = (ms: any) => new Promise((res) => setTimeout(res, ms));
-    async function getNews() {
-      alert("It may take a few minutes to load the news articles due to rate limits. Please be patient.")
-      const token: any = await chrome.storage.sync.get(["token"]);
-      const res = await fetch(
-        "https://simple-backend-0b6s.onrender.com/api/news",
-        {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer " + token.token,
-          },
-        }
-      );
+  async function processArticle() {
+    setLoading(true);
+    const token: any = await chrome.storage.sync.get(["token"]);
+    const res = await fetch("https://simple-backend-0b6s.onrender.com/api/news-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token.token,
+      },
+      body: JSON.stringify({
+        url: articleLink,
+      }),
+    });
+    const news = await res.json();
 
-      const news = await res.json();
-      for (let i = 0; i < news.length; i++) {
-        if (i !== 0) await delay(30000);
-        const res1 = await fetch(
-          "https://simple-backend-0b6s.onrender.com/api/rhyme",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + token.token,
-            },
-            body: JSON.stringify({
-              headline: news[i].headline,
-            }),
-          }
-        );
-
-        const rhymingHeadline = await res1.json();
-        await delay(30000);
-
-
-        const res2 = await fetch(
-          "https://simple-backend-0b6s.onrender.com/api/bias",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + token.token,
-            },
-            body: JSON.stringify({
-              content: news[i].content,
-            }),
-          }
-        );
-        const biasSummary = await res2.json();
-
-        setNewsArticles((prev) => {
-          return [
-            ...prev,
-            {
-              id: news[i].id,
-              headline: news[i].headline,
-              rhymingHeadline: rhymingHeadline,
-              biasSummary: biasSummary,
-              articleLink: news[i].link,
-            },
-          ];
-        });
+    const res1 = await fetch(
+      "https://simple-backend-0b6s.onrender.com/api/rhyme",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token.token,
+        },
+        body: JSON.stringify({
+          headline: news.headline,
+        }),
       }
-      setLoading(false);
-    }
-    if (isLoggedin) getNews();
-  }, [isLoggedin]);
+    );
+
+    const rhymingHeadline = await res1.json();
+
+    const res2 = await fetch(
+      "https://simple-backend-0b6s.onrender.com/api/bias",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token.token,
+        },
+        body: JSON.stringify({
+          content: news.content,
+        }),
+      }
+    );
+
+    const biasSummary = await res2.json();
+
+    setNewsArticle({
+      id: news.id,
+      headline: news.headline,
+      rhymingHeadline: rhymingHeadline,
+      biasSummary: biasSummary,
+      articleLink: articleLink,
+    });
+
+    setLoading(false);
+  }
 
   return (
-    <div className="main-page-container">
-      <div className="news-container">
-        {newsArticles.map((article) => (
-          <div key={article.id} className="news-card">
-            <h2>{article.rhymingHeadline}</h2>
-            <p className="rhyming-headline">{article.headline}</p>
-            <p className="bias-summary">{article.biasSummary}</p>
-            <a
-              href={article.articleLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read Article
-            </a>
+    <div className="news-article-container">
+      <label htmlFor="articleLink">Enter Article Link:</label>
+      <input
+        type="text"
+        id="articleLink"
+        value={articleLink}
+        onChange={(e) => setArticleLink(e.target.value)}
+      />
+      <button onClick={processArticle}>Process Article</button>
+      {loading && (
+        <div className="spinner">
+          <TailSpin color="#00BFFF" height={80} width={80} />
+        </div>
+      )}
+      {newsArticle && (
+        <>
+          <h2>Article Information</h2>
+          <div className="article-info">
+            <p>
+              <strong>Rhyming Headline:</strong> {newsArticle.rhymingHeadline}
+            </p>
+            <p>
+              <strong>Original Headline:</strong> {newsArticle.headline}
+            </p>
+            <p>
+              <strong>Bias:</strong> {newsArticle.biasSummary}
+            </p>
+            <p>
+              <strong>Article Link:</strong>{" "}
+              <a
+                href={newsArticle.articleLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {newsArticle.articleLink}
+              </a>
+            </p>
           </div>
-        ))}
-      </div>
-        {loading && <Grid></Grid>}
+        </>
+      )}
     </div>
   );
 };
-
 export default MainPage;
